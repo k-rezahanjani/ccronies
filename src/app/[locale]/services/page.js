fe50315ad6f35f74716be0582eaf915e4cad7a0a
k-rezahanjani <kaveh.rh69@gmail.com>
2024-09-21 14:40:42 +0330

    service direction fixed

--- src/app/[locale]/services/page.js
+++ src/app/[locale]/services/page.js
@@ -1,10 +1,17 @@
 "use client"
 import React from "react"
 import DigitalMarketing from "@/components/sections/DigitalMarketing"
+import { useLocale } from "next-intl";
 
 export default function Page() {
+
+    const locale = useLocale();
+
+    const direction = locale === 'fa' ? 'rtl' : 'ltr';
+    const textAlign = locale === 'fa' ? 'right' : 'left';
+
     return (
-        <section className="w-full">
+        <section className="w-full" style={{ direction, textAlign }}>
             <div className=" bg-black pb-20 sm:hidden"></div>
             <DigitalMarketing />
         </section>
